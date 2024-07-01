c4d3d3cc32bb87f14bbc2d9208d6d7e321123250
Markus Maal <markus.maal@gmail.com>
2024-07-02 00:21:57 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsBootscreenSimulatorPlus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // set items of OsList here
+            OsList.ItemsSource = new List<string> { "Windows 98", "Windows XP" };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (OsList.SelectedItem.ToString()) {
+                case "Windows 98":
+                    Win98 w98 = new Win98();
+                    w98.WindowStyle = (FullscreenBox.IsChecked ?? false) ? WindowStyle.None : WindowStyle.SingleBorderWindow;
+                    w98.WindowState = (FullscreenBox.IsChecked ?? false) ? WindowState.Maximized : WindowState.Normal;
+                    w98.VBox.Stretch = (StretchBox.IsChecked ?? false) ? Stretch.Fill : Stretch.Uniform;
+                    w98.Show();
+                    break;
+                default:
+                    MessageBox.Show("Not implemented!", "Simulate function", MessageBoxButton.OK, MessageBoxImage.Hand);
+                    break;
+            }
+        }
+    }
+}
---
+++ Win98.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowsBootscreenSimulatorPlus
+{
+    /// <summary>
+    /// Interaction logic for Win98.xaml
+    /// </summary>
+    public partial class Win98 : Window
+    {
+        public Win98()
+        {
+            InitializeComponent();
+        }
+
+        private void AnimateBar(int time)
+        {
+            DoubleAnimation da = new DoubleAnimation
+                (0, -640, new Duration(TimeSpan.FromSeconds(time)));
+
+            StackPanel[] images = [Slit];
+            DoubleAnimation[] animations = [da];
+            TranslateTransform[] transforms = [new TranslateTransform()];
+            for (int i = 0; i < images.Length; i++)
+            {
+                images[i].RenderTransform = transforms[i];
+                images[i].RenderTransformOrigin = new Point(0, 0);
+                animations[i].RepeatBehavior = RepeatBehavior.Forever;
+            }
+            for (int i = 0; i < images.Length; i++) {
+                transforms[i].BeginAnimation(TranslateTransform.XProperty, animations[i]);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AnimateBar(10);
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (WindowStyle == WindowStyle.SingleBorderWindow)
+            {
+                WindowStyle = WindowStyle.None;
+                WindowState = WindowState.Maximized;
+            } else
+            {
+                WindowState = WindowState.Normal;
+                WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key == Key.Escape) || (e.Key == Key.F7))
+            {
+                Close();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
